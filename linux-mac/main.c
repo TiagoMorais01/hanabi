0960a24f37baf763bdb4d24c20f6228ed25836df
Tomas <chumbosilva200@gmail.com>
2020-01-15 17:18:29 +0000

    Changes

--- linux-mac/main.c
+++ linux-mac/main.c
@@ -501,6 +501,9 @@ void init(Deck deckM, Deck trash, Player ai, Player jog, Pilha pi, Log log[], in
             PlayAI(deckM, trash, ai, jog, pi, log, &lives, &tips, &nc, &nt, &np);
             ShowCardAI(deckM, trash, ai, jog, pi, log, lives, tips, nc, nt, np);
             play = 1;
+            if (ultimasJ > 0){
+                ultimasJ--;
+            }
         }
 
         if (ultimasJ == 0){
